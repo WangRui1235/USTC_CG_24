5a5556ec5d04a1b7e67d1f3420c53defdfc77294
WangRui1235 <2086395437@qq.com>
2024-03-20 23:11:55 +0800

    1

---
+++ Framework2D/src/assignments/2_ImageWarping/warping_RBF.cpp
@@ -0,0 +1,37 @@
+#include "comp_warping.h"
+
+
+#include <Eigen/Dense>
+
+// std::pair<int, int>CompWarping::fisheye_warping(int x, int y, int width, int height)
+// {
+//     float center_x = width / 2.0f;
+//     float center_y = height / 2.0f;
+//     float dx = x - center_x;
+//     float dy = y - center_y;
+//     float distance = std::sqrt(dx * dx + dy * dy);
+
+//     // Simple non-linear transformation r -> r' = f(r)
+//     float new_distance = std::sqrt(distance) * 10;
+
+//     if (distance == 0)
+//     {
+//         return { static_cast<int>(center_x), static_cast<int>(center_y) };
+//     }
+//     // (x', y')
+//     float ratio = new_distance / distance;
+//     int new_x = static_cast<int>(center_x + dx * ratio);
+//     int new_y = static_cast<int>(center_y + dy * ratio);
+
+//     return { new_x, new_y };
+// }
+// }  // namespace USTC_CG
+
+namespace USTC_CG
+{
+    std::pair<int, int> warping_IDW::warping_func(int x, int y, int width, int height)
+    {
+       return {x, y};
+    }
+
+}// namespace USTC_CG
